982fa4f22d7534559fd9a11dc5b9209d27cb6cb3
Mr.Dong <tstark@AcdeMacBook-Pro.local>
2020-02-25 21:10:52 +0800

    Update axios.js

--- src/network/axios.js
+++ src/network/axios.js
@@ -5,7 +5,7 @@ export default function axios(option) {
   return new Promise((resolve, reject) => {
     // 1.创建axios的实例
     const instance = originAxios.create({
-      baseURL: 'http://106.54.54.237:8000/api/hy',
+      baseURL: 'http://106.54.54.237:8000',
       timeout: 5000
     });
 
